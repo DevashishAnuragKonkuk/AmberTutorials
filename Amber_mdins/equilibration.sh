2b5de85482eeaa83ba7fd381e9e9ec3237e8cdbc
Devashish Das <dasdevashishdas@gmail.com>
2018-06-25 15:56:55 +0900

    Add files via upload

---
+++ Amber_mdins/equilibration.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Started Equilibration on `date` "
+do_parallel="pmemd.cuda"
+
+prmtop="solvcomplex.prmtop"
+coords="solvcomplex"
+
+
+MDINPUTS=(01.min 02.equil 03.min 04.min 05.min 06.equil 07.equil 08.equil 09.equil)
+
+for input in ${MDINPUTS[@]}; do
+
+ $do_parallel -O -i ${input}.mdin -o ${input}.mdout -p $prmtop -c ${coords}.rst7 -ref ${coords}.rst7 -x ${input}.trj -inf ${input}.info -r ${input}.rst7
+ coords=$input
+done
+
+echo "Finished Equilibration on `date` "
---
+++ Amber_mdins/prod.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "Started Production on `date` "
+#do_parallel="mpirun pmemd.MPI"
+do_cuda="mpirun pmemd.MPI"
+
+prmtop="solvcomplex.prmtop"
+coords="09.equil"
+
+
+MDINPUTS=(10.prod)
+
+for input in ${MDINPUTS[@]}; do
+
+ $do_cuda -O -i ${input}.mdin -o ${input}.mdout -p $prmtop -c ${coords}.rst7 -ref ${coords}.rst7 -x ${input}.trj -inf ${input}.info -r ${input}.rst7
+ coords=$input
+done
+
+echo "Finished Production on `date` "
